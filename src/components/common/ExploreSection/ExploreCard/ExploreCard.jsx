5467a1711bf5e346b1984ed137cc5e2a0eddbd56
Kamni-123 <singhkamni2002@gmail.com>
2024-02-28 21:38:42 +0530

    update

--- src/components/common/ExploreSection/ExploreCard/ExploreCard.jsx
+++ src/components/common/ExploreSection/ExploreCard/ExploreCard.jsx
@@ -1,28 +1,44 @@
 import React from 'react'
+import { FaStar } from "react-icons/fa";
 
 
 const ExploreCard = ({restaurant}) => {
     const name = restaurant?.info?.name ?? "";
     const coverImg = restaurant?.info?.image?.url;
-    // const rating = restaurant?.info?.rating?.rating_text; 
+    const rating = restaurant?.info?.rating?.rating_text; 
     const deliveryTime = restaurant?.order?.deliveryTime;
-    // const approximate = restaurant?.info?.cfo?.text;
+    const approximate = restaurant?.info?.cfo?.text;
     // const offers = restaurant?.bulkOffers ?? [];
-    // const cuisines = restaurant?.info?.cuisine?.map((item)=> item.name).slice(0, 3);
+    const cuisines = restaurant?.info?.cuisine?.map((item)=> item.name).slice(0,2);
     // const bottomContainers = restaurant?.bottomContainers;
-    const goldOff = restaurant?.gold?.text;
-    // eslint-disable-next-line
+    // const goldOff = restaurant?.gold?.text;
+    
     // const proOff = offers.length>1?offers[0].text:null;
-    // // eslint-disable-next-line
+   
     // const discount = offers.length>1?offers[1].text: offers.length === 1 ?offers[1].text:null;
   return (
-    <div className='explore-card my-12  flex justify-center items-center cursor-pointer py-10 px-4 border border-solid border-gray-300 rounded-lg'>
+    <div className='explore-card hover:shadow-2xl my-4 pb-20 flex justify-center items-center cursor-pointer py-10 px-4 border border-solid border-gray-300 rounded-lg'>
         <div className=' explore-card-cover h-80 w-80  '>
-            <img  src={coverImg} className='explore-card-cover-img h-80 w-80 box-border rounded-lg ' alt={name}/>
-            <div>{deliveryTime}</div>
-            {/* {proOff && <div className=''>{proOff}</div>} */}
+            <img  src={coverImg} className='explore-card-cover-img h-80 w-80 mb-2 box-border rounded-lg ' alt={name}/>
+            <div className='flex items-center justify-between '>
+            <div className=' '>{name}</div>
+            
+            {/* <div className=''>{goldOff}</div> */}
+            
+            
+            
+            <div className=' text-white flex bg-green-600 '>< FaStar/>{rating}</div>
+            </div>
+            <div className='flex items-center justify-between '>
+            <div className="">{cuisines}</div>
+            <div className=''>{approximate}</div>
+            </div>
+            <div className='flex justify-end gap-72'>{deliveryTime}</div>
+            {/* <div className=''>{bottomContainers}</div>  */}
+            
+            {/* {proOff && <div className=''>{proOff}</div>}
             {goldOff && <div className='absolute'>{goldOff}</div>}
-            {/* {discount && <div className=''>{discount}</div>} */}
+            {discount && <div className=''>{discount}</div>} */}
         </div>
     </div>
     
--- src/components/common/ExploreSection/ExploreSections.jsx
+++ src/components/common/ExploreSection/ExploreSections.jsx
@@ -6,7 +6,7 @@ const ExploreSections = ({ list, collectionName }) => {
   return (
     <div className="max-width">
       <div className="collection-title text-2xl  font-semibold pl-4  text-gray-500">{collectionName}</div>
-      <div className="explore-grid flex flex-wrap justify-between">
+      <div className="explore-grid flex flex-wrap mt-0 mb-0 justify-around">
         {list.map((restaurant) => {
           return <ExploreCard restaurant={restaurant} />;
         })}
